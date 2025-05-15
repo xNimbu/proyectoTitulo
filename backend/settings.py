182b74c79da4c6996a7c3e43a31dfbe3f79a4bc7
xNimbu <lui.illanes@duocuc.cl>
2025-05-15 04:08:27 -0400

    Elimina corsheaders del middleware y de las aplicaciones instaladas

--- backend/settings.py
+++ backend/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'core',
-    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -51,7 +50,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'backend.urls'
