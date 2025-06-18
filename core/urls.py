75580888aed38b81cc15f2ca9f5af97c55646549
xNimbu <lui.illanes@duocuc.cl>
2025-06-18 16:58:49 -0400

    Corrige un error tipográfico en la definición de la ruta de publicaciones en urls.py

--- core/urls.py
+++ core/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path("profile/pets/<str:pet_id>/", views.pet_detail),
     path("profile_list/", views.profile_list, name="profile_list"),
     path("chat/<str:room>/history/", views.chat_history, name="chat_history"),
-    path("posts/", views.posts),x
+    path("posts/", views.posts),
     path("posts/user/<str:uid>/", views.posts_by_user),
     path("posts/<str:post_id>/", views.post_detail),
     path("posts/<str:post_id>/comments/", views.comments),             # GET = listar, POST = agregar
