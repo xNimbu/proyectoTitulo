e60bcf51a4daaf4108f1d495b325e92f0c17c483
xNimbu <lui.illanes@duocuc.cl>
2025-06-24 21:50:11 -0400

    Refactor ChatMessage: renombrar campo timestamp a created y agregar método __str__ para mejor representación. Actualizar render.yaml para incluir makemigrations en el comando de construcción.

--- core/models.py
+++ core/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 class ChatMessage(models.Model):
-    room = models.CharField(max_length=100)
-    user = models.CharField(max_length=100)
+    room    = models.CharField(max_length=100)
+    user    = models.CharField(max_length=100)
     message = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        ordering = ('timestamp',)
+    def __str__(self):
+        return f'[{self.created:%Y-%m-%d %H:%M}] {self.user}: {self.message}'
