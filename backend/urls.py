ab002e9f1985671f6786d3ca83734d6aadb59359
xNimbu <lui.illanes@duocuc.cl>
2025-07-01 01:07:26 -0400

    Agregar archivo index.html: crear la estructura básica de la página principal con enlaces a Chat Prueba y obtener Token Firebase.

--- backend/urls.py
+++ backend/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
     path('chat_prueba/', TemplateView.as_view(template_name='chat_prueba.html'), name='chat_prueba'),
